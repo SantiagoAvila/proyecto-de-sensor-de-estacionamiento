7ba70a88eb7350564519511607444bb05bedf4c9
SantiagoAvila <savila1810@gmail.com>
2019-04-29 14:46:13 -0400

    comentando los pasos alcanzados

--- sensor de estacionamiento.py
+++ sensor de estacionamiento.py
@@ -27,24 +27,29 @@ receptor=13
 ##para el sensor numero 2 definimos los pines en la raspberry
 disparador2=16
 receptor2=18
-GPIO.setmode(GPIO.BOARD)                              ###configuramos los pines del sensor en la raspebrry para  
-GPIO.setup(disparador, GPIO.OUT)                      ### para ambos sensores
-GPIO.setup(receptor,GPIO.IN)
-GPIO.setup(disparador2,GPIO.OUT)
-GPIO.setup(receptor2,GPIO.IN)
+GPIO.setmode(GPIO.BOARD)                #configuramos los pines del sensor en la raspebrry para  
+GPIO.setup(disparador, GPIO.OUT)        # para ambos sensores
+GPIO.setup(receptor,GPIO.IN)			#
+GPIO.setup(disparador2,GPIO.OUT)		#
+GPIO.setup(receptor2,GPIO.IN)			#
 #### Definimos una funcion para la lectura del espacio de estacionamiento
 GPIO.input(receptor)=lugar1
 GPIO.input(receptor2)=lugar2
 def det_espacio():
     GPIO.output(disparador,False)       ##configuramos los tiempos que trabajaran los respectivos emisores
     GPIO.output(disparador2,False)      ##arrancando en estado apagado
-    time.sleep(2*10**-6)
-    GPIO.output(disparador,True)
-    GPIO.output(disparador2,True)
-    time.sleep(10*10**-6)
-    while GPIO.input(receptor)==0:
-        empieza1=time.time()
-    while GPIO.ipunt(receptor)==1:
-        termina1=time.time()
-	duracion1= termina1-empieza1
-	
+    time.sleep(2*10**-6)				##
+    GPIO.output(disparador,True)		##
+    GPIO.output(disparador2,True)		##
+    time.sleep(10*10**-6)				##
+    while GPIO.input(receptor)==0:		###definimos el contador para los respectivos receptores de los sensores
+        empieza1=time.time()			###y asi obtener el tiempo de recorrido de la señal
+    while GPIO.ipunt(receptor)==1:		###
+        termina1=time.time()			###
+	duracion1= termina1-empieza1		###
+	while GPIO.input(receptor2)==0:		###
+		empieza2=time.time()			###
+	while GPIO.input(receptor2)==1:		###
+		termina2=time.time()			###
+	duracion2=termina2-empieza2			###
+
