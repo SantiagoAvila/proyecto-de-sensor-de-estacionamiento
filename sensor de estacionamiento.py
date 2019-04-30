4e1c04ffeae37df510c519c73e5d8cc85a250d3c
SantiagoAvila <savila1810@gmail.com>
2019-04-30 10:47:40 -0400

    funciona para dos lugares

--- sensor de estacionamiento.py
+++ sensor de estacionamiento.py
@@ -2,6 +2,7 @@
 """
 ESTO DEBE CORRER EN LA RASPBERRY PARA QUE FUNCIONE
 
+Tutores Claves: Willi Bobadilla @WilliBobadilla y Lore 
 Objetivo:
         en el presente archivo se intentara realizar una pagina web para el monitoreo de vacancias 
         de estacionamientos funcional en un local determinado utilizando raspberry, que responda en
@@ -18,50 +19,85 @@ Consideraciones:
         3)El cliente recibira una pantalla que le indicara por lugar el estatus de cada estacionamiento de
         bateria dentro del local como ( texto libre+ mas color verde) o (texto ocupado + color rojo)
 """
-import RPi.GPIO as GPIO
 import time #se necesita para usar las funciones de tiempo
-
-GPIO.setmode(GPIO.BOARD) #Queremos usar la numeracion de la placa raspberry
-#Definimos los dos pines del sensor que hemos conectado: disparador y Eco
-disparador = 11
-Eco = 13
-#Hay que configurar ambos pines del HC-SR04
-GPIO.setup(disparador, GPIO.OUT)
-GPIO.setup(Eco, GPIO.IN)
-#Para leer la distancia del sensor al lugar del estacionamiento, creamos una funcion
-def detectar_lugar():
+import RPi.GPIO as GPIO
  
-   GPIO.output(disparador, False) #apagamos el pin disparador
+GPIO.setmode(GPIO.BOARD) #definimos la configuracion de la raspberry para utilizar la numeracion de los pines
+disparador1 = 11
+Eco1 = 13
+disparador2 = 3
+Eco2 = 5
+ 
+#Hay que configurar ambos pines del HC-SR04 para ambos sensores
+GPIO.setup(disparador1, GPIO.OUT)
+GPIO.setup(Eco1, GPIO.IN)
+GPIO.setup(disparador2,GPIO.OUT)
+GPIO.setup(Eco2,GPIO.IN)
+#definimos la funcion para el sensor numero 1 
+def detectar_lugar1():
+ 
+   GPIO.output(disparador1, False) #apagamos el pin disparador1
    time.sleep(2*10**-6) #esperamos dos microsegundos
-   GPIO.output(disparador, True) #encendemos el pin disparador
+   GPIO.output(disparador1, True)#encendemos el pin 
    time.sleep(10*10**-6) #esperamos diez microsegundos
-   GPIO.output(disparador, False) #y lo volvemos a apagar
-  #empezaremos a contar el tiempo cuando el pin Eco se encienda
-   while GPIO.input(Eco) == 0:
-      start = time.time()
-
-   while GPIO.input(Eco) == 1:
-      end = time.time()
+   GPIO.output(disparador1, False)#y lo volvemos a apagar
  
+  #empezaremos a contar el tiempo cuando el pin Eco1 se encienda
+   while GPIO.input(Eco1) == 0:
+      start_1 = time.time()
+ 
+   while GPIO.input(Eco1) == 1:
+      end_1 = time.time()
+      
    #La duracion del pulso del pin Eco sera la diferencia entre
    #el tiempo de inicio y el final
-   duracion = end-start
+   duracion_1 = end_1-start_1
+ 
+   #Este tiempo viene dado en  segundos. Si lo pasamos
+   #a microsegundos, podemos aplicar directamente las formulas
+   #encontradas en los ejemplos extraidos
+   duracion1 = duracion_1*10**6
+   medida1 = duracion1/58 #hay que dividir por la constante que pone en la documentacion, 
+   #nos dara la distancia en cm
+   return medida1
+#definimos la funcion para el lugar2
+def detectar_lugar2():
+   GPIO.output(disparador2, False) #apagamos el pin dispardor y seguimos los mismos pasos del lugar 1
+   time.sleep(2*10**-6)
+   GPIO.output(disparador2, True)
+   time.sleep(10*10**-6) 
+   GPIO.output(disparador2, False)
+  #empezaremos a contar el tiempo cuando el pin Eco2 se encienda
+   while GPIO.input(Eco2) == 0:
+      start_2 = time.time()
+ 
+   while GPIO.input(Eco2) == 1:
+      end_2 = time.time()
+      
+   #La duracion del pulso del pin Eco2 sera la diferencia entre
+   #el tiempo de inicio2 y el final2
+   duracion_2 = end_2-start_2
  
    #Este tiempo viene dado en segundos. Si lo pasamos
    #a microsegundos, podemos aplicar directamente las formulas
-   duracion = duracion*10**6
-   medida = duracion/58 #hay que dividir por la constante que pone en la documentacion, nos dara la distancia en cm
-   return medida
-    #por ultimo, vamos a mostrar el resultado por pantalla
- 
+   #de la documentacion
+   duracion2 = duracion_2*10**6
+   medida2 = duracion2/58   #hay que dividir por la constante que pone en la documentacion, 
+                            #nos dara la distancia en cm
+   return medida2
 #Bucle principal del programa, lee el sensor. Se sale con CTRL+C
 while True:
    try:
-      d=detectar_lugar()
-      if 73<=d<=75:
-         print("libre")
+      d1=detectar_lugar1()              #ahora guardamos los valores retornados al llamar las funciones
+      d2=detectar_lugar2()              # para luego con los if relacionar para definir si esta o no libre 
+      if d1>=70:                        #para esta maqueta se hara en miniatura por lo cual los valores son
+         print("libre espacio 1")       #realmente muy pequeños pero si es para un estacionamiento estandar
+      else:                             #se deben usar los valores de la descripcion 
+         print("ocupado espacio 1")
+      if 70<=d2:
+         print("libre espacio 2")
       else:
-         print("ocupado")
-      time.sleep(0.7)
+         print("ocupado espacio 2")
+      time.sleep(2.0)                   #por ultimo le damos un sleep para reinicir la funcion 
    except KeyboardInterrupt:
-      break
+      break                             #ponemos un break para detener el programa
