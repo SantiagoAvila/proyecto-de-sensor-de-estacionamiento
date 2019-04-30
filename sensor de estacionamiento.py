adfd3c48dbb5e2c8bd8180b3b20039e05663fcec
SantiagoAvila <savila1810@gmail.com>
2019-04-30 08:28:41 -0400

    cambios a simplificar parahacer funcionar

--- sensor de estacionamiento.py
+++ sensor de estacionamiento.py
@@ -25,44 +25,45 @@ import time
 disparador= 11
 receptor=13
 ##para el sensor numero 2 definimos los pines en la raspberry
-disparador2=16
-receptor2=18
+#disparador2=16
+#receptor2=18
 GPIO.setmode(GPIO.BOARD)                #configuramos los pines del sensor en la raspebrry para  
 GPIO.setup(disparador, GPIO.OUT)        # para ambos sensores
 GPIO.setup(receptor,GPIO.IN)			#
-GPIO.setup(disparador2,GPIO.OUT)		#
-GPIO.setup(receptor2,GPIO.IN)			#
+#GPIO.setup(disparador2,GPIO.OUT)		#
+#GPIO.setup(receptor2,GPIO.IN)			#
 #### Definimos una funcion para la lectura del espacio de estacionamiento
 def det_espacio():
     GPIO.output(disparador,False)
-    GPIO.output(disparador2,False)
+ #   GPIO.output(disparador2,False)
     time.sleep(2*10**-6)
     GPIO.output(disparador,True)
-    GPIO.output(disparador2,True)
+  #  GPIO.output(disparador2,True)
     time.sleep(10*10**-6)
     GPIO.output(disparador,False)
-    GPIO.output(disparador2,False)
-    while 0==GPIO.input(receptor):          #realizando los calculos de timepo para ambos sensores 
+   # GPIO.output(disparador2,False)
+    while GPIO.input(receptor)==0:          #realizando los calculos de timepo para ambos sensores 
         empieza1=time.time()
-    while 1==GPIO.input(receptor):
+    while GPIO.input(receptor)==1:
         terminar1=time.time()
-    while 0==GPIO.input(receptor2):
-        empieza2=time.time()
-        while 1==GPIO.input(receptor2):
-        termina2=time.time()
+   # while GPIO.input(receptor2)==0:
+    #    empieza2=time.time()
+ #       while GPIO.input(receptor2)==1:
+#        termina2=time.time()
     tiempo1=terminar1-empieza1
-    tiempo2=termina2-empieza2
+#    tiempo2=termina2-empieza2
     distancia1=(tiempo1*10**6)/58            #calculando la distancia en el lugar1
-    distancia2=(tiempo2*10**6)/58           #calculando la distancia en el lugar2
-    if distancia1==195:                     ##para lugar1
-        return "Libre espacio 1"            ##
-    elif distancia1!=195:                    ##
-        return "Ocupado espacio 2"          ##
-    if distancia2==195:                     ##para lugar 2
-        return "Libre espacio 2"
-    elif distancia2!=195:
-            return "Ocupado espacio 2"
+#    distancia2=(tiempo2*10**6)/58           #calculando la distancia en el lugar2
+#    if distancia1==195:                     ##para lugar1
+     #   return "Libre espacio 1"            ##
+    #elif distancia1!=195:                    ##
+     #   return "Ocupado espacio 2"          ##
+    #if distancia2==195:                     ##para lugar 2
+     #   return "Libre espacio 2"
+    #elif distancia2!=195:
+     #       return "Ocupado espacio 2"
     ## en el bucle principal 
+    print "2%f" %distancia1
     while True:
         det_espacio()
         time.sleep(0.5)
